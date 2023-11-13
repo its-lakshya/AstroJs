50d1d46924f72adc200e3d9268847d21eb0ac98f
Lakshya Kumar <kumarlakshya101@gmail.com>
2023-11-14 00:31:41 +0530

    trying to host

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,11 @@ import netlify from "@astrojs/netlify/functions";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://its-lakshya.github.io',
-  base: '/AstroJs-Landing-Page',
-
+  output: "server",
+  adapter: netlify({
+    edgeMiddleware: true
+  })
 });
+
+// site: 'https://its-lakshya.github.io',
+//   base: '/AstroJs-Landing-Page',
